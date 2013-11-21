cce7569ca56ab8ce754fbc6b6b63c20a203d160b
Jay H <mobeets@gmail.com>
2013-11-21 17:33:31 -0600

    errors since i keep hitting rate limit

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from twython import Twython
 
 from story import screen_name, tweet_ranges
 
-from dotenv import DotEnv
-environ = DotEnv('.env')
+from dotenv import Dotenv
+environ = Dotenv('.env')
 # environ = os.environ
 
 CONSUMER_KEY = environ['TWITTER_CONSUMER_KEY']
@@ -42,21 +42,32 @@ def save_to_json(tweets, outfile):
     
 def get_out_while_you_still_can(tweets, outfile):
     fname, ext = os.path.splitext(outfile)
-    outfile = fname + '-'.join([str(i) for i in tweets.keys()]) + ext
+    indices = [str(i) for i in sorted(tweets.keys())]
+    if not indices:
+        print 'No tweets saved.'
+        return
+    print 'Saving tweets from indices {0}'.format(', '.join(indices))
+    outfile = fname + '_' + '-'.join(indices) + ext
     save_to_json(tweets, outfile)
 
 def main(outfile='tmp.json', min_index=4):
     handle = twitter_handle()
     tweets = {}
+    no_errors = True
     for row in tweet_ranges:
         if min_index and row['index'] < min_index:
             continue
         assert row['index'] not in tweets
+        print 'Fetching tweets in index {0}...'.format(row['index'])
         try:
             tweets[row['index']] = tweets_in(handle, row['tweet_start_id'], row['tweet_end_id'])
         except:
             get_out_while_you_still_can(tweets, outfile)
-    save_to_json(tweets, outfile)
+            no_errors = False
+            break
+    if no_errors:
+        save_to_json(tweets, outfile)
+        print 'All tweets saved.'
 
 if __name__ == '__main__':
     main()
